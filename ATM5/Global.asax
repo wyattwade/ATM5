07728b4dc1bf3a29501ad2934f305e90922e906e
Wyatt Elliott <wyattelliott27@gmail.com>
2017-06-07 23:13:37 -0700

    Add project files.

---
+++ ATM5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ATM5.MvcApplication" Language="C#" %>
---
+++ ATM5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ATM5.Startup))]
+namespace ATM5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
