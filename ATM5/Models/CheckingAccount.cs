b0ba1dae99a1b01ced09c19a94f40c7985b4713d
Wyatt Elliott <wyattelliott27@gmail.com>
2017-06-08 23:11:52 -0700

    Chapter3_BeforeDatabaseObjects

---
+++ ATM5/Models/CheckingAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ATM5.Models
+{
+    public class CheckingAccount
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"\d{6,10}", ErrorMessage = "Account # must be 6 to 10 digits.")]
+        [Display(Name="Account #")]
+        public string AccountNumber { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public string Name {
+            get
+            {
+                return string.Format("{0} {1}", this.FirstName, this.LastName);
+            }
+        }
+
+        [DataType(DataType.Currency)]
+        public decimal Balance { get; set; }
+
+
+    }
+}
